a62d5261edabf676c52c57b49638447286a52269
JungJinUk <82055169+dsa04156@users.noreply.github.com>
2022-06-03 15:47:34 +0900

    Create 숫자의개수.py

---
+++ 숫자의개수.py
@@ -0,0 +1,17 @@
+A=int(input())
+B = int(input())
+C = int(input())
+
+sum = A*B*C
+sum_list = list(map(int,str(sum)))
+print(sum_list.count(0))
+print(sum_list.count(1))
+print(sum_list.count(2))
+print(sum_list.count(3))
+print(sum_list.count(4))
+print(sum_list.count(5))
+print(sum_list.count(6))
+print(sum_list.count(7))
+print(sum_list.count(8))
+print(sum_list.count(9))
+
